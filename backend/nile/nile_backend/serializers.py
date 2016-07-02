e08b954eb4f2f9b99f0cdc2a3d2b9f8190d9ee58
Max Thonagel <m.thonagel@campus.tu-berlin.de>
2016-07-02 16:30:45 +0200

    Added Package Serializer'

--- backend/nile/nile_backend/serializers.py
+++ backend/nile/nile_backend/serializers.py
@@ -6,3 +6,14 @@ class UserSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     created_at = serializers.DateTimeField()
     updated_at = serializers.DateTimeField()
+
+class PackageSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    deliverer = UserSerializer()
+    purchaser = UserSerializer()
+    recipient = UserSerializer()
+    status = serializers.CharField(max_length=31)
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
+
+
