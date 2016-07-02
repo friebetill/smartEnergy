853dfb4db72c187f75541781cbc2be381e0f603a
Kolja Esders <kolja.esders@movinga.de>
2016-07-02 15:56:23 +0200

    Added REST endpoint for users.

--- backend/nile/nile_backend/views.py
+++ backend/nile/nile_backend/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from nile_backend.models import Package, User
+from nile_backend.serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 def index(request):
   return HttpResponse("Hello, world. You're at the polls index.")
+
+class Users(APIView):
+  def get(self, request, format=None):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+  def post(self, request, format=None):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
