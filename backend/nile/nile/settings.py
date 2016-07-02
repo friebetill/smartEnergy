94793288007dd3fc19656e82269859490b1b08c4
Kolja Esders <kolja.esders@movinga.de>
2016-07-02 15:00:03 +0200

    Added nile_backend app to project settings.

--- backend/nile/nile/settings.py
+++ backend/nile/nile/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nile_backend.apps.NileBackendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
