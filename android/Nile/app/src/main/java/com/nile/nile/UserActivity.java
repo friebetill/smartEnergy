01be225291af244712ac08e6328f1a4f7bf07b3b
Friederike Fischer <fischer.1@campus.tu-berlin.de>
2016-07-02 17:41:30 +0200

    Implement executePost for request to the server

--- android/Nile/app/src/main/java/com/nile/nile/UserActivity.java
+++ android/Nile/app/src/main/java/com/nile/nile/UserActivity.java
@@ -5,6 +5,15 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class UserActivity extends AppCompatActivity {
 
     @Override
@@ -12,11 +21,57 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        String orderedItems = excutePost("", "");
+
+        //Data data = new Gson().fromJson(orderedItems, Data.class);
+
         TextView textView = new TextView(this);
         textView.setTextSize(25);
         textView.setText("Amazon 15m");
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.LinearLayout1);
         layout.addView(textView);
+    }
+
+    public String excutePost(String targetURL, String urlParameters) {
+        HttpURLConnection connection = null;
+        try {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setRequestProperty("Content-Length",
+                    Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.close();
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if not Java 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 }
