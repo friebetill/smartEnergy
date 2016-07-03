bee020e422959ef59eed9f30fdcd35a6ac076636
Max Thonagel <m.thonagel@campus.tu-berlin.de>
2016-07-03 13:18:44 +0200

    removed mistakes from demo

--- demo/generate_addresses.py
+++ demo/generate_addresses.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def constructApiUrl(street,houseNumber,city,country):
+def constructApiUrl(street,houseNumber,city,country, postcode):
     app_id = "BdJCV1gAjGDkP2dixWRu"
     app_code = "MV0PdJIEhcOYveK3eILRUA"
     street = street.replace("ß", "ss")
@@ -9,19 +9,61 @@ def constructApiUrl(street,houseNumber,city,country):
     apiStreet = ""
     for str in streetParts:
         apiStreet += str + "%20"
-    return  "geocode.json?housenumber="+ houseNumber + "&street=" + apiStreet + "&city=" + city + "&country=" + country + "&gen=8" + "&app_id=" + app_id + "&app_code=" + app_code;
+    return  "geocode.json?housenumber="+ houseNumber + "&street=" + apiStreet + "&city=" + city + "&postalCode="+ postcode + "&country=" + country + "&gen=8" + "&app_id=" + app_id + "&app_code=" + app_code;
 
 def add_addr(street, city, country, zip_code):
     our_server = "http://54.93.34.46"
-    name =["Brett Bowman","Traci Morris","Henry Hill","Nathan Jacobs","Luke Carpenter","Jaime Reyes","Devin Pearson","Angelina Romero","Clint Andrews" , "Michele Arnold"]
+    name =["Brett Bowman","Traci Morris","Henry Hill","Nathan Jacobs","Luke Carpenter","Jaime Reyes","Devin Pearson","Angelina Romero","Clint Andrews" , "Michele Arnold", "Kyong Levy",
+            "Darcy Torchia",
+            "Terese Sifuentes",
+            "Willie Waltrip",
+            "Elenor Bonebrake",
+            "Jazmin Slough",
+            "Bev Brugman",
+            "Jackson Dejarnette",
+            "Obdulia Scheu",
+            "Lauryn Bellefeuille",
+            "Merrilee Fulcher",
+            "Narcisa Fallis",
+            "Merle Roe",
+            "Nerissa Baumgartner",
+            "Nicolette Mares",
+            "Patria Tarver",
+            "Lashaun Line",
+            "Samira Zepp",
+            "Celesta Blais",
+            "Carlee Febus",
+            "Sybil Hempel",
+            "Fran Mcclendon",
+            "Randy Mccallie",
+            "Annabell Fenderson",
+            "Bridgette Chiasson",
+            "Una Hobson",
+            "Ollie Shropshire",
+            "Isiah Petersen",
+            "Emelia Eastman",
+            "Raphael Boysen",
+            "Chandra Sankey",
+            "Myrtle Bohl",
+            "Leone Old",
+            "Sherri Ort",
+            "Diedre Grabert",
+            "Kiley Stock",
+            "Maryjo Coppage",
+            "Lilian Hussein",
+            "Mammie Holst",
+            "Eleanore Pinegar",
+            "Elayne Bordner",
+            "Cayla Hayman",
+            "Ahmad Gaymon" ]
     for i in range(1,len(name)):
-        r = requests.get("https://geocoder.cit.api.here.com/6.2/"+constructApiUrl("Charelottenstrasse", str(i), "Berlin", "Germany"))
+        r = requests.get("https://geocoder.cit.api.here.com/6.2/"+constructApiUrl(street, str(i), "Berlin", "Germany",zip_code))
 
         print(r)
 
         res = r.json()
-        lat = res["Response"]["View"][0]["Result"][0]["Location"]["NavigationPosition"][0]["Latitude"]
-        lng = res["Response"]["View"][0]["Result"][0]["Location"]["NavigationPosition"][0]["Longitude"]
+        lat = res["Response"]["View"][0]["Result"][0]["Location"]["DisplayPosition"]["Latitude"]
+        lng = res["Response"]["View"][0]["Result"][0]["Location"]["DisplayPosition"]["Longitude"]
 
 
         headers = {"Content-Type":"application/json"}
@@ -29,7 +71,7 @@ def add_addr(street, city, country, zip_code):
         print("lat"+str(lat))
         print("lng"+str(lng))
 
-        payload = {"name":name[0],"type":"client"}
+        payload = {"name":name[i],"type":"client"}
         r = requests.post(our_server+"/users/", data=json.dumps(payload), headers=headers)
         print(r)
         res = r.json()
@@ -46,11 +88,12 @@ def main():
     street = "Charlottenstrasse"
     city = "Berlin"
     country = "Germany"
-    zip_code = "10969"
 
-    add_addr("Charlottenstrasse", city, country, zip_code)
-    add_addr("Alte Jakobstraße", city, country, zip_code)
-    add_addr("Marktgrafenstraße", city, country, zip_code)
+    add_addr("Charlottenstrasse", city, country, "10969")
+    add_addr("Alte Jakobstraße", city, country, "10969")
+    add_addr("Marktgrafenstraße", city, country, "10969")
+    add_addr("Rudi-Dutschke-Straße",city, country, "10969")
+
 
 
 if __name__ == "__main__":
