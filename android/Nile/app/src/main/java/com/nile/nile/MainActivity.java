d31f8620f1910a52a4fee749ff8c8d82f64e8875
Friederike Fischer <fischer.1@campus.tu-berlin.de>
2016-07-03 02:54:17 +0200

    Switch directly to right part

--- android/Nile/app/src/main/java/com/nile/nile/MainActivity.java
+++ android/Nile/app/src/main/java/com/nile/nile/MainActivity.java
@@ -1,30 +1,21 @@
 package com.nile.nile;
 
-import android.location.Location;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.here.android.mpa.common.GeoCoordinate;
-import com.here.android.mpa.search.Address;
-import com.here.android.mpa.search.ErrorCode;
-import com.here.android.mpa.search.GeocodeRequest;
-import com.here.android.mpa.search.ResultListener;
-import com.here.android.mpa.search.ReverseGeocodeRequest;
-
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.nile.nile.service.GPSTracker;
-import com.nile.nile.service.ReverseGeoCodeListener;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,49 +28,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        mTracker = new GPSTracker(this);
-        if(mTracker.canGetLocation()) {
-            mLatitude = mTracker.getLatitude();
-            mLongitude = mTracker.getLongitude();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences pref = this.getSharedPreferences("Share", Context.MODE_PRIVATE);
+        int isUser = pref.getInt("isUser", -1);
+        int currentID = pref.getInt("currentID", -1);
+        Log.d("IsUser",String.valueOf(isUser));
+        Log.d("CurrentID",String.valueOf(currentID));
+
+        if(isUser == -1){
+            mTracker = new GPSTracker(this);
+            if(mTracker.canGetLocation()) {
+                mLatitude = mTracker.getLatitude();
+                mLongitude = mTracker.getLongitude();
+            } else {
+                mTracker.showSettingsAlert();
+            }
+            Toast.makeText(getApplicationContext(),"Current location: Latitude: " + mLatitude + "\n" + "Longitude: " + mLongitude, Toast.LENGTH_LONG).show();
+        } else if (isUser == 1){
+            Intent intent;
+            if(currentID == -1){
+                intent = new Intent(this, UserRegisterActivity.class);
+            } else {
+                intent = new Intent(this, UserActivity.class);
+            }
+            startActivity(intent);
         } else {
-            mTracker.showSettingsAlert();
+            Intent intent = null;
+            if(currentID == -1){
+                //intent = new Intent(this, DelivererRegisterActivity.class);
+            } else {
+                intent = new Intent(this, DelivererActivity.class);
+            }
+            startActivity(intent);
         }
-        Toast.makeText(getApplicationContext(),"Current location: Latitude: " + mLatitude + "\n" + "Longitude: " + mLongitude, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /** Called when the user clicks the User button */
     public void setUserView(View view) {
-        Intent intent = new Intent(this, UserRegisterActivity.class);
+        SharedPreferences pref = this.getSharedPreferences("Share", Context.MODE_PRIVATE);
+        int currentID = pref.getInt("currentID", -1);
+
+        Intent intent;
+        if(currentID == -1){
+            intent = new Intent(this, UserRegisterActivity.class);
+        } else {
+            intent = new Intent(this, UserActivity.class);
+        }
+
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putInt("isUser", 1);
+        edit.commit();
+
         startActivity(intent);
     }
 
     /** Called when the user clicks the Deliverer button */
     public void setDelivererView(View view) {
         Intent intent = new Intent(this, DelivererActivity.class);
+
+        SharedPreferences pref = this.getSharedPreferences("Share", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putInt("isUser", 0);
+        edit.commit();
+
         startActivity(intent);
     }
 }
--- android/Nile/app/src/main/java/com/nile/nile/UserActivity.java
+++ android/Nile/app/src/main/java/com/nile/nile/UserActivity.java
@@ -7,28 +7,19 @@ import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.gson.Gson;
 import com.nile.nile.model.NilePackage;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.CookieHandler;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-
-
-
 public class UserActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,94 +39,27 @@ public class UserActivity extends AppCompatActivity {
         orders.setText("Orders");
         layout.addView(orders);
 
-        String json =
-                "["
-                + "{"
-                +"    \"id\": 1,"
-                +"    \"deliverer\": {"
-                +"      \"id\": 1,"
-                +"      \"name\": \"KarlsruheUser\","
-                +"      \"created_at\": \"2016-07-02T14:18:54.855487Z\","
-                +"      \"updated_at\": \"2016-07-02T14:41:35.280191Z\""
-                +"    },"
-                +"    \"purchaser\": {"
-                +"      \"id\": 4,"
-                +"      \"name\": \"DHL\","
-                +"      \"created_at\": \"2016-07-02T16:17:35.245816Z\","
-                +"      \"updated_at\": \"2016-07-02T16:17:35.246141Z\""
-                +"    },"
-                +"    \"recipient\": null,"
-                +"    \"status\": \"open\","
-                +"    \"created_at\": \"2016-07-02T16:29:13.824609Z\","
-                +"    \"updated_at\": \"2016-07-02T16:29:13.824686Z\""
-                +  "}"
-                +"]";
-
-        int userID = 4;
-
-        //String orderedItems = executeGET("http://54.93.34.46/users/" + userID + "/packages/", "");
         new executeGETThread().execute();
 
-        //NilePackage[] packages = new Gson().fromJson(orderedItems, NilePackage[].class);
-        /*NilePackage[] packages = new Gson().fromJson(json, NilePackage[].class);
+        Log.d("TOKEN", FirebaseInstanceId.getInstance().getToken());
+
+    }
+
+    private void updateOrders(String json) {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.LinearLayout1);
+        NilePackage[] packages = new Gson().fromJson(json, NilePackage[].class);
 
         for(int i = 0; i < packages.length; i++){
             TextView textView = new TextView(this);
             textView.setTextSize(25);
-            textView.setText(packages[i].getSender() + " " + (int) packages[i].getEstimatedDeliveryTime() + "m");
+            textView.setText(packages[i].getSender() + " " + (int) packages[i].getMins_until_delivery() + "m");
 
             layout.addView(textView);
-        }
-
-        Log.d("TOKEN",FirebaseInstanceId.getInstance().getToken());*/
-
-    }
-
-    public String excutePost(String targetURL, String urlParameters) {
-        HttpURLConnection connection = null;
-        try {
-            //Create connection
-            URL url = new URL(targetURL);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-
-            connection.setRequestProperty("Content-Length",
-                    Integer.toString(urlParameters.getBytes().length));
-            connection.setRequestProperty("Content-Language", "en-US");
-
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-
-            //Send request
-            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-            wr.writeBytes(urlParameters);
-            wr.close();
-
-            //Get Response
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder(); // or StringBuffer if not Java 5+
-            String line;
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            return response.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
         }
     }
 
     public String executeGET(String targetURL, String urlParameters) {
         HttpURLConnection connection = null;
-        //CookieHandler.setDefault( new CookieManager( null, CookiePolicy.ACCEPT_ALL ) );
         try {
             //Create connection
             URL url = new URL(targetURL);
@@ -163,7 +87,6 @@ public class UserActivity extends AppCompatActivity {
         return sb.toString();
     }
 
-
     class executeGETThread extends AsyncTask<String, Void, String> {
 
         protected String doInBackground(String... urls) {
@@ -179,6 +102,7 @@ public class UserActivity extends AppCompatActivity {
             } else {
                 Log.d("ExecuteThread", result);
             }
+            updateOrders(result);
         }
     }
 
