69acc40ea7f0aa85bde5c535a584cdc0751af914
Friederike Fischer <fischer.1@campus.tu-berlin.de>
2016-07-03 08:07:16 +0200

    Updates location now every two seconds

--- android/Nile/app/src/main/java/com/nile/nile/DelivererActivity.java
+++ android/Nile/app/src/main/java/com/nile/nile/DelivererActivity.java
@@ -2,6 +2,11 @@ package com.nile.nile;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -17,12 +22,14 @@ import com.google.firebase.iid.FirebaseInstanceId;
 
 import com.google.gson.Gson;
 
-public class DelivererActivity extends AppCompatActivity {
+public class DelivererActivity extends AppCompatActivity implements LocationListener {
 
     private double mLatitude;
     private double mLongitude;
 
     GPSTracker mTracker;
+
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +38,7 @@ public class DelivererActivity extends AppCompatActivity {
         SharedPreferences pref = this.getSharedPreferences("Share", Context.MODE_PRIVATE);
         int currentID = pref.getInt("currentID", -1);
         mTracker = new GPSTracker(this, currentID);
-        if(mTracker.canGetLocation()) {
+        if (mTracker.canGetLocation()) {
             mLatitude = mTracker.getLatitude();
             mLongitude = mTracker.getLongitude();
         } else {
@@ -45,6 +52,21 @@ public class DelivererActivity extends AppCompatActivity {
         nextAddr.setText("Next Address");
 
         layout.addView(nextAddr);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                2000,   // 3 sec
+                0, this);
     }
 
     @Override
@@ -90,5 +112,28 @@ public class DelivererActivity extends AppCompatActivity {
 
         Log.d("TOKEN", FirebaseInstanceId.getInstance().getToken());
 
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String str = "Latitude: "+location.getLatitude()+"Longitude: "+location.getLongitude();
+
+        Log.d("Location", str);
+        Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(getBaseContext(), "Gps turned on ", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(getBaseContext(), "Gps turned off ", Toast.LENGTH_LONG).show();
     }
 }
